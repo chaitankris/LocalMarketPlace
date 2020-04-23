ae0de388a410aeed2097cc1799fffed195d51e0f
Aayush_Verma <aayush_verma1996@yahoo.com>
2020-04-23 23:08:33 +0530

    initial commit

---
+++ src/main/java/com/spe/local_market_place/LocalMarketPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.spe.local_market_place;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalMarketPlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalMarketPlaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spe/local_market_place/LocalMarketPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spe.local_market_place;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalMarketPlaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
